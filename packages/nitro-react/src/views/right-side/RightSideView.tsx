1a3ea61b4e38e14cc533758c99c5f0c678c74ad6
nicollassilva <lyod.hp@gmail.com>
2026-08-02 23:55:00 -0300

    Seasonal currency

--- packages/nitro-react/src/views/right-side/RightSideView.tsx
+++ packages/nitro-react/src/views/right-side/RightSideView.tsx
@@ -1,9 +1,13 @@
 import { PurseView } from "./purse/PurseView";
+import { SeasonalView } from "./purse/seasonal/SeasonalView";
 
 export const RightSideView = () => {
     return (
         <div className="nitro-right-side-container">
             <PurseView />
+            <div className="notifications-container">
+                <SeasonalView />
+            </div>
         </div>
     );
 }
