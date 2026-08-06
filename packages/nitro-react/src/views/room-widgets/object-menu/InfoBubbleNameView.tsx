18a74229a0d0760a88f3cb942305eb25a22e73f1
billsonnn <bill@billsonnn.com>
2026-08-05 23:04:23 -0400

    Updates

--- packages/nitro-react/src/views/room-widgets/object-menu/InfoBubbleNameView.tsx
+++ packages/nitro-react/src/views/room-widgets/object-menu/InfoBubbleNameView.tsx
@@ -12,7 +12,7 @@ export const InfoBubbleNameView = (props: InfoBubbleNameProps) => {
     if (!nameData) return null;
 
     return (
-        <Bubble className="flex items-center justify-center w-17.5 py-1.5" variant="0" tintColor="#3d3d3d">
+        <Bubble className="flex items-center justify-center min-w-17.5 py-1.5" variant="0" tintColor="#3d3d3d">
             <span className="text-style-u-regular font-[11px] text-white">{nameData.name}</span>
         </Bubble>
     );
