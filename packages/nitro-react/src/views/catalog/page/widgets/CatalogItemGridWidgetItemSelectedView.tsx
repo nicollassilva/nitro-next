7946ef2655e834e9d5d1b4fb96e70ae70ef67dda
billsonnn <bill@billsonnn.com>
2026-08-18 21:20:54 -0400

    Updates

--- packages/nitro-react/src/views/catalog/page/widgets/CatalogItemGridWidgetItemSelectedView.tsx
+++ packages/nitro-react/src/views/catalog/page/widgets/CatalogItemGridWidgetItemSelectedView.tsx
@@ -17,7 +17,7 @@ export const CatalogItemGridWidgetItemSelectedView = (props: CatalogItemGridWidg
     if (!offer || !product) return null;
 
     return (
-        <Border variant="3" tintColor="#63c5e9" className="flex items-center justify-center p-0.5 min-h-16.25">
+        <Border variant="3" tintColor="#63c5e9" className="flex items-center justify-center p-0.5">
             <Border variant="3" className="size-full flex flex-col items-center gap-0.75 py-0.75 overflow-hidden">
                 <Image wrapperClassName="min-w-[32px] min-h-[32px] max-w-[32px] max-h-[32px]" src={iconUrl} />
                 <CatalogItemGridWidgetItemPriceView offer={offer} />
